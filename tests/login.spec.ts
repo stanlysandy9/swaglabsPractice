2b32fa3dc7b6b3cb732aeac1f52f6c7757a84456
Stanly Nimmakuri <stanly.nimmakuri@csgsol.net>
2026-03-13 11:20:50 +0530

    new tab added

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,20 @@
+import { assert } from "node:console";
 import { expect, test } from "../fixtures/loginFixture";
 import { HomePage } from "../Pages/homePage";
 
-test("TC001 @login to application", async ({ mainPage, homePage, page }) => {
+test("TC001 @login to application", async ({ mainPage, homePage, page, context }) => {
     //await mainPage.goToLoginPage();
     //await mainPage.loginToApp();
     //await homePage.getAllProducts();
     await page.locator(('button[name^="add-to-cart"]')).first().click();
 //await page.getByRole('link', { name: /1/ });
     await expect(page.locator('.inventory_details_price')).toHaveText('$29.99');
+    const newTab= await context.newPage();
+await newTab.goto('https://www.google.com');
+await expect(newTab).toHaveTitle("Google");
+const pages= context.pages();
+await pages[0].bringToFront();
 await homePage.logout();
 });
+
+
